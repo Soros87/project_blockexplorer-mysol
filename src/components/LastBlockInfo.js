3363d77e2570b6133caaa9acfb2ca199f82e64d8
Soros87 <timkwan87@gmail.com>
2023-10-23 13:55:04 +0800

    set up the table css

---
+++ src/components/LastBlockInfo.js
@@ -0,0 +1,151 @@
+import { Alchemy, Network } from 'alchemy-sdk';
+import React, { useEffect, useState } from 'react';
+
+const settings = {
+  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+  network: Network.ETH_MAINNET,
+};
+
+const alchemy = new Alchemy(settings);
+
+const LastBlockInfo = () => {
+
+  const [blockNumber, setBlockNumber] = useState();
+
+  useEffect(() => {
+    async function getBlockNumber() {
+      setBlockNumber(await alchemy.core.getBlockNumber());
+    }
+
+    getBlockNumber();
+  });
+
+  return (
+
+    <><div className="App">
+      Block Number: {blockNumber}
+    </div>
+      <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-3">
+                Block Info
+              </th>
+              <th scope="col" class="px-6 py-3">
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                Block Number
+              </th>
+              <td class="px-6 py-4">
+                placeholder
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                hash
+              </th>
+              <td class="px-6 py-4">
+              placeholder
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                parentHash
+              </th>
+              <td class="px-6 py-4">
+                placeholder
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                hash
+              </th>
+              <td class="px-6 py-4">
+              placeholder
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                logsBloom
+              </th>
+              <td class="px-6 py-4">
+              placeholder
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                timestamp
+              </th>
+              <td class="px-6 py-4">
+              placeholder
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                nonce
+              </th>
+              <td class="px-6 py-4">
+              placeholder
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                difficulty
+              </th>
+              <td class="px-6 py-4">
+              placeholder
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                gasLimit
+              </th>
+              <td class="px-6 py-4">
+              placeholder
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                gasUsed
+              </th>
+              <td class="px-6 py-4">
+              placeholder
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                size
+              </th>
+              <td class="px-6 py-4">
+              placeholder
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                miner
+              </th>
+              <td class="px-6 py-4">
+              placeholder
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                transactions
+              </th>
+              <td class="px-6 py-4">
+              placeholder
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      </>
+  )
+}
+
+export default LastBlockInfo
---
+++ src/pages/Home.js
@@ -0,0 +1,39 @@
+import { Alchemy, Network } from 'alchemy-sdk';
+import React, { useEffect, useState } from 'react'
+import LastBlockInfo from '../components/LastBlockInfo'
+// Refer to the README doc for more information about using API
+// keys in client-side code. You should never do this in production
+// level code.
+const settings = {
+  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+  network: Network.ETH_MAINNET,
+};
+
+
+// In this week's lessons we used ethers.js. Here we are using the
+// Alchemy SDK is an umbrella library with several different packages.
+//
+// You can read more about the packages here:
+//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
+const alchemy = new Alchemy(settings);
+
+
+
+const Home = () => {
+
+  const [blockNumber, setBlockNumber] = useState();
+
+  useEffect(() => {
+    async function getBlockNumber() {
+      setBlockNumber(await alchemy.core.getBlockNumber());
+    }
+
+    getBlockNumber();
+  });
+
+  return (
+    <LastBlockInfo/>
+  )
+}
+
+export default Home
